19354221a9460ac13e21ec308501e36da909c2e5
eSociety97 <46420217+eSociety97@users.noreply.github.com>
2019-01-06 09:32:57 -0600

    Add files via upload

---
+++ Enrollar_Huella.ino
@@ -0,0 +1,217 @@
+/******************Almacenar Huellas en el Sensor Dactilar*******************************************
+ *                                                                                                  *
+ *    Este sketch nos servira para poder almacenar nuestras huellas dactilares en nuestro Sensor    *
+ *  tendremos espacio para 162 huellas diferentes que es lo que nos permite.                        * 
+ *                                                                                                  *
+ *  Este codigo lo podemos encontrar como ejemplo de la libreria Adafruit_Fingerprint.h             *
+ ****************************************************************************************************/
+
+/***********LIBRERIAS************/
+#include <SoftwareSerial.h>
+#include <Adafruit_Fingerprint.h>
+
+uint8_t huella(int num);
+/************OBJETOS**************/
+SoftwareSerial puerto(7,6);
+Adafruit_Fingerprint lector = Adafruit_Fingerprint(&puerto);
+
+void setup(){
+  Serial.begin(9600);                 //Iniciamos la Comunicacion Serial
+  Serial.println("Buscando Sensor");  
+  lector.begin(57600);                //Iniciamos la Comunicacion con el Sensor De Huellas
+  
+  //Verificamos si el sensor de huellas esta conectado
+  if(lector.verifyPassword()){    
+  Serial.println("Sensor de huella encontrado :D !!");  
+  }
+  else{
+  Serial.println("Sensor de Huella no encontrado :c !!");
+  while(1);
+  }
+}
+
+void loop(){
+  Serial.println("Escriba el # ID que desea guardar");  
+  int num = 0;  
+  while(true){  
+    while(! Serial.available());
+    char c = Serial.read();   //cambiar a entero
+    if(! isdigit(c)) break;
+    num *= 10;
+    num += c - '0';
+  }
+  Serial.print("Almacenando # ID");
+  Serial.println(num);
+    
+  while(! huella(num));        
+  }
+  
+  uint8_t huella(int num){  
+    int i = -1;    
+    Serial.println("Esperando huella");
+    
+    while(i != FINGERPRINT_OK){      
+    i = lector.getImage();    
+    
+    switch(i){
+    
+      case FINGERPRINT_OK:
+      Serial.println("Huella leida");
+      break;
+      
+      case FINGERPRINT_NOFINGER:
+      Serial.println("Esperando Huella...");
+      break;
+      
+      case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Error de Comunicacion");
+      break;
+      
+      case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Error de huella");
+      break;
+      
+      default: 
+      Serial.println("Error desconocido");
+      break;
+    }
+    }
+    
+    i = lector.image2Tz(1);
+    
+    switch(i){
+    
+      case FINGERPRINT_OK:
+      Serial.println("Huella Convertida");
+      break;
+      
+      case FINGERPRINT_IMAGEMESS:
+      Serial.println("Imagen demasiado Sucia");
+      return i;
+      
+      case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Error de comunicacion");
+      return i;
+      
+      case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Caracteristicas de huella no encontradas");
+      return i;
+      
+      case FINGERPRINT_INVALIDIMAGE:      
+      Serial.println("Caracteristicas de huella no encontradas");
+      return i;
+      
+      default: 
+      Serial.println("Error desconocido");
+      return i;      
+    }
+    
+    Serial.println("Remueva el dedo");
+    delay(2000);
+    i = 0;
+   
+    while(i != FINGERPRINT_NOFINGER){
+      i = lector.getImage();    
+    }
+    
+    i = -1;
+    Serial.println("Coloque de nuevo el dedo ");
+    
+    while(i != FINGERPRINT_OK){      
+    i = lector.getImage();
+    
+    switch(i){
+    
+      case FINGERPRINT_OK:
+      Serial.println("Huella tomada");
+      break;
+      
+      case FINGERPRINT_NOFINGER:
+      Serial.println("Esperando huella");
+      break;
+      
+      case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Error de comunicacion");
+      break;
+      
+      case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Error de HUELLA");
+      break;
+      
+      default:
+      Serial.println("Error Desconocido");
+      break;      
+    }
+    }
+    
+    i = lector.image2Tz(2);
+    
+    switch(i) {
+    
+     case FINGERPRINT_OK:
+     Serial.println("Huella tomada"); 
+     break;
+     
+     case FINGERPRINT_IMAGEMESS:
+     Serial.println("Imagen demasiado Sucia");
+     return i;
+     
+     case FINGERPRINT_PACKETRECIEVEERR:
+     Serial.println("Error de Comunicacion");
+     return i;
+     
+     case FINGERPRINT_FEATUREFAIL:
+     Serial.println("Caracteristicas de huella no encontradas");
+     return i;
+     
+     case FINGERPRINT_INVALIDIMAGE:     
+     Serial.println("Caracteristicas de huella no encontradas");
+     return i;
+     
+     default:
+     Serial.println("Error desconocido");
+     return i;     
+    }
+  
+     i = lector.createModel();
+  
+     if(i == FINGERPRINT_OK){
+     Serial.println("Las huellas coinciden");
+     }
+     else if(i == FINGERPRINT_PACKETRECIEVEERR){
+     Serial.println("Error de Comunicacion");
+     return i;
+     }
+     else if(i == FINGERPRINT_ENROLLMISMATCH){
+     Serial.println("Las huellas dactilares no coinciden");
+      return i;  
+      }
+      else{
+      Serial.println("Error desconocido");
+      return i;  
+      }
+      
+      Serial.print("ID "); Serial.println(num);
+      i = lector.storeModel(num);
+      
+      if(i == FINGERPRINT_OK){
+        Serial.println("Huella Almacenada");      
+      }
+      else if(i == FINGERPRINT_PACKETRECIEVEERR){
+      Serial.println("Error de Comunicacion");
+      return i;     
+      }
+      else if(i == FINGERPRINT_BADLOCATION){
+      Serial.println("No se pudo almacenar en esa ubicacion");
+      return i;
+      }
+      else if(i == FINGERPRINT_FLASHERR){
+        Serial.println("Error al escribir para flashear");
+        return i;            
+      }
+      else{
+      Serial.println("Error desconocido");
+      return i;
+      } 
+  }   
+  
---
+++ Parqueo_Inteligente.ino
@@ -0,0 +1,147 @@
+/*************************************PARQUEO INTELIGENTE CON SENSOR DACTILAR******************************************************
+*                                                                                                                                 *
+*     Este sistema depende de una confirmacion de una huella ya almacenada en el sensor dactilar, si la huella coincide           *
+*  dara la apertura del servomotor por 10 segundos. Luego de los diez segundos el servomotor bloqueara el paso.                   *
+*                                                                                                                                 *
+*  Para lograr esto necesitaremos la libreria del sensor dactilar que nos proporciona Adafruit Adafruit_Fingerprint.h             *
+*                                                                                                                                 *
+*  Tambien la libreria SoftwareSerial y Servo que incluye ya el IDE de Arduino                                                                  *
+*                                                                                                                                 *
+*  Primero cargaremos el codigo "Enrolar Huella" de la libreria Adafruit_Fingerprint.h para poder almacenar nuestras huellas   *
+*  tenemos un espacio de 162 huellas diferentes.                                                                                  *
+*                                                                                                                                 *
+*  Luego de cargar el codigo de "Enrolar Huella" ahora si ya cargaremos nuestro codigo.                                                   *
+*                                                                                                                                 *
+*                                                                                                                                 *
+*                                                 #NuncaDejenDeCrear                                                              *
+***********************************************************************************************************************************/                                                                                                                              
+
+/**********LIBRERIAS************/
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+int getFingerprintIDez();
+
+/*************************OBJETOS****************************/
+SoftwareSerial mySerial(2, 3);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+Servo puerta;
+
+void setup() {
+  puerta.attach(9);                   //Indicamos que conectaremos el Servo al pin 9 
+  puerta.write(90);                   //Le damos una posicion Inicial de Cerrado
+  Serial.begin(9600);                 //Iniciamos la Comunicacion Serial
+  Serial.println("prueba de dedo");
+  finger.begin(57600);                //Iniciamos la Comunicacion con el Sensor 
+  //Verificamos que el sensor este conectado c:
+  if (finger.verifyPassword()) {
+    Serial.println("Sensor de huella digital encontrado!");
+  } else {
+    Serial.println("No encontré el sensor de huellas dactilares:(");
+    while (1);
+  }
+}
+
+void loop() {
+  //Llamamos el metodo Obtener ID Huella Digital 
+    getFingerprintIDez();     
+    delay(50);      
+}
+
+
+
+uint8_t getFingerprintID() {
+  uint8_t p = finger.getImage();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Imagen Tomada");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println("Sin Dedo Detectado");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Error de Comunicacion");
+      return p;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Error de Imagen");
+      return p;
+    default:
+      Serial.println("Error Desconocido");
+      return p;
+  }
+  // OK, Exito!
+  
+  p = finger.image2Tz();
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Imagen Convertida");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Imagen Demasiado Sucia");
+      return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Error de Comunicacion");
+      return p;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("No he podido encontrar las caracteristicas de la huella dactilar");
+      return p;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("No he podido encontrar las caracteristicas de la huella dactilar");
+      return p;
+    default:
+      Serial.println("Error Desconocido");
+      return p;
+  }
+  
+  // OK converted!
+  p = finger.fingerFastSearch();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Encontré una coincidencia de huellas!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Error de Comunicacion");
+    return p;
+  } else if (p == FINGERPRINT_NOTFOUND) {
+    Serial.println("No se encontro coincidencia");
+    return p;
+  } else {
+    Serial.println("Error Desconocido");
+    return p;
+  } 
+  
+  // Encontré una coincidencia.
+  Serial.print("Encontrado ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" Con confidencia de: "); Serial.println(finger.confidence); 
+}
+
+int getFingerprintIDez() {
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK)  return -1;
+
+  p = finger.fingerFastSearch();
+  if (p != FINGERPRINT_OK)  return -1;
+  
+  //Aqui se hacen las comparaciones para abrir 
+  //colocaran las huellas que quieren que Aperturen el Servomotor
+  if(finger.fingerID == 1){
+  Serial.println("Bienvenido"); 
+  puerta.write(0);   //Aperturamos el Servo 
+  delay(10000);      //Por 10 segundos 
+  puerta.write(90);   //Luego lo regresamos a su posicion de Cerrado
+  }
+    if(finger.fingerID == 0){
+  Serial.println("Bienvenido"); 
+  puerta.write(0);   //Aperturamos la puerta accionando el Servo 
+  delay(10000);      //Por 10 segundos 
+  puerta.write(90);  //Luego lo regresamos a su posicion de Cerrado
+  }
+
+  
+  // Encontré una coincidencia.
+  Serial.print("Encontrado ID #"); Serial.print(finger.fingerID); 
+  Serial.print(" Con confidencia de: "); Serial.println(finger.confidence);
+  return finger.fingerID; 
+}
